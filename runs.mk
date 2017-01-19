0b9af026f38541f176de59035b9d5d78b3a443fc
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-01-20 08:54:18 +0900

    fix runs.mk

--- runs.mk
+++ runs.mk
@@ -4,44 +4,44 @@ main:
 	@./scripts/eoolo.$(LANG)
 
 js:
-	@$(MAKE) main LANG=js
+	@$(MAKE) main LANG=$@
 
 rb:
-	@$(MAKE) main LANG=rb
+	@$(MAKE) main LANG=$@
 
 py:
-	@$(MAKE) main LANG=py
+	@$(MAKE) main LANG=$@
 
 pl:
-	@$(MAKE) main LANG=pl
+	@$(MAKE) main LANG=$@
 
 scm:
-	@$(MAKE) main LANG=scm
+	@$(MAKE) main LANG=$@
 
 c:
 	@echo ''
 	@echo '--- eoolo.$@ ---'
-	@gcc ./scripts/eoolo.c
+	@gcc ./scripts/eoolo.$@
 	@./a.out
 	@rm -rf ./a.out
 
 hs:
-	@$(MAKE) main LANG=hs
+	@$(MAKE) main LANG=$@
 
 java:
 	@echo ''
 	@echo '--- eoolo.$@ ---'
-	@javac ./scripts/eoolo.java
+	@javac ./scripts/eoolo.$@
 	@cd scripts; java eoolo
 	@rm -rf ./scripts/eoolo.class
 
 nim:
 	@echo ''
 	@echo '--- eoolo.$@ ---'
-	@nim c -r ./scripts/eoolo.nim 2>/dev/null
+	@nim c -r ./scripts/eoolo.$@ 2>/dev/null
 	@rm -rf ./scripts/eoolo ./scripts/nimcache
 
 go:
 	@echo ''
 	@echo '--- eoolo.$@ ---'
-	@go run ./scripts/eoolo.go
+	@go run ./scripts/eoolo.$@
