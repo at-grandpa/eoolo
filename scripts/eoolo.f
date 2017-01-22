105339930e7893f4e6fa1faf8788b04b1e02b429
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-01-22 21:52:18 +0900

    fix eoolo.f

--- scripts/eoolo.f
+++ scripts/eoolo.f
@@ -1,14 +1,14 @@
       PROGRAM hello
           logical expr1, expr2
-          WRITE (*,*) (expr1().or.expr2())
+          WRITE (*,'(L)') (expr1().or.expr2())
       END
 
       logical FUNCTION expr1() result(r)
-          WRITE (*,*) 'method expr1'
+          WRITE (*,'(A)') 'method expr1'
           r = .TRUE.
       END
 
       logical FUNCTION expr2() result(r)
-          WRITE (*,*) 'method expr2'
+          WRITE (*,'(A)') 'method expr2'
           r = .TRUE.
       END
